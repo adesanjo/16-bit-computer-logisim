ec48c416959031ebb70b89d7a1237a359e5f72cc
adesanjo <adesanjo@ulb.ac.be>
2017-11-02 17:38:23 +0100

    Created microCodeEncoder.py

---
+++ 16 bit computer/data/microCodeEncoder.py
@@ -0,0 +1,54 @@
+file=open("../sheets/Instructions.txt")
+text=file.read()
+file.close()
+lines=[line.strip() for line in text.splitlines()]
+lines=lines[lines.index("Micro Code:")+2:]
+lines=[line for line in lines if len(line)>0]
+hexadec="0123456789abcdef"
+microInstructions={"pc out":0x00000001,
+                   "mar in":0x00000002,
+                  "ram out":0x00000004,
+                     "i in":0x00000008,
+                   "pc inc":0x00000010,
+                   "pc set":0x00000020,
+                   "ram in":0x00000040,
+            "reset counter":0x00000080,
+                     "a in":0x00000100,
+                     "b in":0x00000200,
+                  "alu out":0x00000400,
+                    "ax in":0x00000800,
+                    "bx in":0x00001000,
+                    "cx in":0x00002000,
+                    "dx in":0x00004000,
+                   "ax out":0x00008000,
+                   "bx out":0x00010000,
+                   "cx out":0x00020000,
+                   "dx out":0x00040000,
+                     "halt":0x00080000,
+                   "bp out":0x00100000,
+                    "bp in":0x00200000,
+                   "sp out":0x00400000,
+                    "sp in":0x00800000,
+                   "si out":0x01000000,
+                    "si in":0x02000000,
+                   "di out":0x04000000,
+                    "di in":0x08000000,
+                       "ja":0x10000000,
+                      "jna":0x20000000,
+                       "je":0x30000000,
+                      "jne":0x40000000,
+                       "jb":0x50000000,
+                      "jnb":0x60000000,
+                       "jg":0x70000000,
+                      "jng":0x80000000,
+                       "jl":0x90000000,
+                      "jnl":0xa0000000}
+opcode=""
+instructions={}
+for line in lines:
+    if len(line)>4 and line[0] in hexadec and line[1] in hexadec and line[2] in hexadec and line[3] in hexadec:
+        opcode=line[:4]
+        instructions[opcode]=[]
+    else:
+        instructions[opcode].append(line.split(", "))
+print(instructions)
