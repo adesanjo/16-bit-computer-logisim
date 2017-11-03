dfbedfacc6cf926ec2177d572090931c24b3fe08
adesanjo <adesanjo@ulb.ac.be>
2017-11-03 10:34:14 +0100

    added example code

--- 16 bit computer/programs/compiler.py
+++ 16 bit computer/programs/compiler.py
@@ -1,1 +1,15 @@
 import sys
+
+"""
+give code name with .16b extension
+"""
+
+file=open(sys.argv[1])
+code=file.read()
+file.close()
+
+code=code.splitlines()
+
+file=open(sys.argv[1].replace(".16b",".bin"),"w")
+file.write("v2.0 raw")
+file.close()
