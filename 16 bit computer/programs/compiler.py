be2ee1b3d251c96d7c5a42806f3955a38f26f781
adesanjo <adesanjo@ulb.ac.be>
2017-11-03 10:54:58 +0100

    Started coding compiler.py

--- 16 bit computer/programs/compiler.py
+++ 16 bit computer/programs/compiler.py
@@ -8,7 +8,20 @@ file=open(sys.argv[1])
 code=file.read()
 file.close()
 
+# variables are defined by a -
+vars={}
+# labels are difined by a :
+labels={}
+
 code=code.splitlines()
+code=[line.strip().split() for line in code if len(line.strip())>0]
+
+for line in code:
+    if line[0][0]=="-":
+        vars[line[0]]=-1
+    elif line[0][0]==":":
+        labels[line[0]]=-1
+    
 
 file=open(sys.argv[1].replace(".16b",".bin"),"w")
 file.write("v2.0 raw")
